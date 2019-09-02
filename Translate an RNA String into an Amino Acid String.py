3663370856762347834547231775199890149701
Kitipatteacha <31331922+Kitipatteacha@users.noreply.github.com>
2019-09-02 11:32:54 +0700

    Add files via upload

---
+++ Translate an RNA String into an Amino Acid String.py
@@ -0,0 +1,30 @@
+import fileinput
+amino = {"AAA":"K", "AAC":"N", "AAG":"K", "AAU":"N", 
+                "ACA":"T", "ACC":"T", "ACG":"T", "ACU":"T", 
+                "AGA":"R", "AGC":"S", "AGG":"R", "AGU":"S", 
+                "AUA":"I", "AUC":"I", "AUG":"M", "AUU":"I", 
+
+                "CAA":"Q", "CAC":"H", "CAG":"Q", "CAU":"H", 
+                "CCA":"P", "CCC":"P", "CCG":"P", "CCU":"P", 
+                "CGA":"R", "CGC":"R", "CGG":"R", "CGU":"R", 
+                "CUA":"L", "CUC":"L", "CUG":"L", "CUU":"L", 
+
+                "GAA":"E", "GAC":"D", "GAG":"E", "GAU":"D", 
+                "GCA":"A", "GCC":"A", "GCG":"A", "GCU":"A", 
+                "GGA":"G", "GGC":"G", "GGG":"G", "GGU":"G", 
+                "GUA":"V", "GUC":"V", "GUG":"V", "GUU":"V", 
+
+                "UAA":"", "UAC":"Y", "UAG":"", "UAU":"Y", 
+                "UCA":"S", "UCC":"S", "UCG":"S", "UCU":"S", 
+                "UGA":"", "UGC":"C", "UGG":"W", "UGU":"C", 
+                "UUA":"L", "UUC":"F", "UUG":"L", "UUU":"F"}
+lines = [line.strip() for line in fileinput.input('rosalind_ba4a (2).txt')]
+a = ""
+k = ""
+for i in lines:
+    for j in range (len(i)):
+        a = a + i[j]
+        if((j+1)%3==0 and j!=0):
+            k = k + amino[a]
+            a=''
+print(k)
